a26cbcaabcbbf1aba023b22fd70502aed3ab5b72
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-06-10 22:13:45 +0200

    console test

---
+++ src/donna-console-totext/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace donna_console_totext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
