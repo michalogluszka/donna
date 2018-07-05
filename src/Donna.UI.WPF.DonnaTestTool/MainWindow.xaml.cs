63b8613250be6547afecb4ba42c9882ae2813280
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-07-05 21:27:21 +0200

    hello world TTS

--- src/Donna.UI.WPF.DonnaTestTool/MainWindow.xaml.cs
+++ src/Donna.UI.WPF.DonnaTestTool/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using Donna.Core.AzureSecurity;
+﻿using CognitiveServicesTTS;
+using Donna.Core.AzureSecurity;
 using Donna.Core.AzureSecurity.Providers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -41,12 +45,57 @@ namespace Donna.UI.WPF.DonnaTestTool
             ISubscriptionKeyProvider provider = new SubscriptionKeyEnviromentVariableProvider("SpeechServiceSubscriptionKey");
             var auth = new AzureAuthToken(provider, issueTokenUri);
 
-            string token = auth.GetAccessToken();
+            string accessToken = auth.GetAccessToken();
 
-            Debug.WriteLine(token);
+            Debug.WriteLine(accessToken);
 
+            string requestUri = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1";
+            var cortana = new Synthesize();
 
+            cortana.OnAudioAvailable += PlayAudio;
+            cortana.OnError += ErrorHandler;
 
+            // Reuse Synthesize object to minimize latency
+            cortana.Speak(CancellationToken.None, new Synthesize.InputOptions()
+            {
+                RequestUri = new Uri(requestUri),
+                // Text to be spoken.
+                Text = "Hello, how are you doing?",
+                VoiceType = Gender.Female,
+                // Refer to the documentation for complete list of supported locales.
+                Locale = "en-US",
+                // You can also customize the output voice. Refer to the documentation to view the different
+                // voices that the TTS service can output.
+                // VoiceName = "Microsoft Server Speech Text to Speech Voice (en-US, Jessa24KRUS)",
+                VoiceName = "Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)",
+                // VoiceName = "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)",
+
+                // Service can return audio in different output format.
+                OutputFormat = AudioOutputFormat.Riff24Khz16BitMonoPcm,
+                AuthorizationToken = "Bearer " + accessToken,
+            }).Wait();
+
+        }
+
+        private static void PlayAudio(object sender, GenericEventArgs<Stream> args)
+        {
+            Console.WriteLine(args.EventData);
+
+            // For SoundPlayer to be able to play the wav file, it has to be encoded in PCM.
+            // Use output audio format AudioOutputFormat.Riff16Khz16BitMonoPcm to do that.
+            SoundPlayer player = new SoundPlayer(args.EventData);
+            player.PlaySync();
+            args.EventData.Dispose();
+        }
+
+        /// <summary>
+        /// Handler an error when a TTS request failed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="GenericEventArgs{Exception}"/> instance containing the event data.</param>
+        private static void ErrorHandler(object sender, GenericEventArgs<Exception> e)
+        {
+            Console.WriteLine("Unable to complete the TTS request: [{0}]", e.ToString());
         }
     }
 }
