7bac43aac0ddee1eac3357386844c848db593714
Michal Ogluszka <michal.ogluszka@avanade.com>
2019-02-25 15:33:45 +0100

    first component

---
+++ donna-client-vue/src/components/DonnaMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DonnaMain extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
